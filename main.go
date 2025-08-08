14a21c2cb14fa216b490938875f07172be1a93c1
Justin Trahan <justin@trahan.dev>
2025-08-08 19:11:33 +0000

    gmt

--- main.go
+++ main.go
@@ -4,12 +4,20 @@ Copyright © 2025 justin@trahan.dev
 package main
 
 import (
+	"log"
 	"log/slog"
 
 	"github.com/babbage88/systinker/cmd"
 )
 
+var db *SqliteDb
+
 func main() {
+	db = &SqliteDb{Filename: "systinker.db"}
+	err := db.initDb()
+	if err != nil {
+		log.Fatalf("error initializing sqlite db %s", err.Error())
+	}
 	configureDefaultLogger(slog.LevelDebug)
 	cmd.Execute()
 }
